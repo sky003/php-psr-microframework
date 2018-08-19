5b059d6d92bc722da298df34b412c515d7b4125e
Anton Pelykh <anton.pelykh.dev@gmail.com>
2018-08-19 23:14:17 +0300

    Add codeception module configuration

---
+++ config/container-test.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Builds the service container for test environment.
+ *
+ * This is a part of application's composition root.
+ *
+ * @var \Symfony\Component\DependencyInjection\ContainerBuilder $containerBuilder
+ */
+
+declare(strict_types = 1);
