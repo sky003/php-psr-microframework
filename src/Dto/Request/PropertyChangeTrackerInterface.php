2ae882b902bc9ad65ed2149fbe2a79b1a0b55437
Anton Pelykh <anton.pelykh.dev@gmail.com>
2018-08-21 17:27:53 +0300

    Create the interface to track the properties changes

---
+++ src/Dto/Request/PropertyChangeTrackerInterface.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types = 1);
+
+namespace App\Dto\Request;
+
+/**
+ * Interface to implement the properties change tracker.
+ *
+ * @author Anton Pelykh <anton.pelykh.dev@gmail.com>
+ */
+interface PropertyChangeTrackerInterface
+{
+    /**
+     * Whether the property was changed.
+     *
+     * @param string $propertyName
+     *
+     * @return bool
+     */
+    public function isPropertyChanged(string $propertyName): bool;
+
+    /**
+     * Whether track changes or not.
+     *
+     * @return bool
+     */
+    public function isTrackerEnabled(): bool;
+
+    /**
+     * Enable/disable tracker.
+     *
+     * @param bool $enabled
+     */
+    public function setTrackerEnabled(bool $enabled): void;
+}
